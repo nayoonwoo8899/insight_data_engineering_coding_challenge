155bb8433d26d168b48bac6be168f6dc73cbac72
nayoonwoo8899 <41222469+nayoonwoo8899@users.noreply.github.com>
2018-07-14 20:16:27 -0500

    documentation
    Python 3 and prepare readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # insight_data_engineering_coding_challenge
+
+summarize approach:
+
+
+run instruction: 
+given by instruction page
+https://github.com/InsightDataScience/pharmacy_counting
+
+
+
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #
 # Use this shell script to run the Python program
 #
-python ./src/pharmacy_counting.py ./input/itcont.txt ./output/top_cost_drug.txt
+python3 ./src/pharmacy_counting.py ./input/itcont.txt ./output/top_cost_drug.txt
